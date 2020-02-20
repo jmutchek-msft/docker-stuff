534894a343c8074a00364a030bc4005f8143d5a1
jmutchek <john5@mutchek.com>
2020-02-20 16:18:08 -0500

    add shared-mem project

---
+++ windows/shared-mem/shared-mem-03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace shared_mem_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("testmap"))
+                {
+
+                    Mutex mutex = Mutex.OpenExisting("testmapmutex");
+                    mutex.WaitOne();
+
+                    using (MemoryMappedViewStream stream = mmf.CreateViewStream(2, 0))
+                    {
+                        BinaryWriter writer = new BinaryWriter(stream);
+                        writer.Write(1);
+                    }
+                    mutex.ReleaseMutex();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Memory-mapped file does not exist. Run Process A first, then B.");
+            }
+        }
+    }
+}
